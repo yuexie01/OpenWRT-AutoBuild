2d0ce876b497d80f896e57f7e0884cc9b842d953
yueying23 <106509653+yueying23@users.noreply.github.com>
2024-08-17 20:23:02 +0800

    Create Settings.sh

---
+++ Scripts/Settings.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#删除冲突插件
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+
+# 在OpenWrt源码目录中移除添加的自定义feeds
+sed -i "/src-git smpackage https://github.com/kenzok8/small-package/d" ./feeds.conf.default
+
+#修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+
+#修正连接数（by ベ七秒鱼ベ）
+# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+#根据源码来修改
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  #修改默认时间格式
+  sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+fi
